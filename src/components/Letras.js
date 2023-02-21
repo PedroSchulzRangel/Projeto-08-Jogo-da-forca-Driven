44d00d915eb34282c748905217897bcc711620ea
Pedro Schulz e Silva Rangel <psrangel5002@hotmail.com>
2023-02-20 21:18:29 -0300

    Pré-jogo implementado

--- src/components/Letras.js
+++ src/components/Letras.js
@@ -2,7 +2,7 @@ export default function Letras(){
     const alfabeto = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
     return (
     <div class="letras">
-    {alfabeto.map(letraDoAlfabeto => <div>{letraDoAlfabeto}</div>)}
+    {alfabeto.map(letraDoAlfabeto => <button disabled>{letraDoAlfabeto}</button>)}
     </div>
     );
 }
