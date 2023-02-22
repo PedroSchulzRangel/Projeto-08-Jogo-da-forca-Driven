846b8a7d6fe01e22a16a0a53664789bbabf88945
Pedro Schulz e Silva Rangel <psrangel5002@hotmail.com>
2023-02-22 17:48:23 -0300

    botão de escolher palavra implementado

--- src/components/App.js
+++ src/components/App.js
@@ -7,10 +7,15 @@ export default function App() {
 
 const [habilitarLetras, setHabilitarLetras] = useState(true);
 const [contarErros, setContarErros] = useState(0);
+const [palavraEscolhida, setPalavraEscolhida] = useState([]);
 
   return (
   <div className="container">
-      <Jogo setHabilitarLetras={setHabilitarLetras} contarErros={contarErros} palavras={palavras}/>
+      <Jogo setHabilitarLetras={setHabilitarLetras} 
+      contarErros={contarErros} 
+      palavras={palavras} 
+      palavraEscolhida={palavraEscolhida} 
+      setPalavraEscolhida={setPalavraEscolhida}/>
       <Letras habilitarLetras={habilitarLetras}/>
   </div>
   );
--- src/components/Jogo.js
+++ src/components/Jogo.js
@@ -1,21 +1,36 @@
 import forca0 from "../assets/forca0.png";
+import forca1 from "../assets/forca1.png";
+import forca2 from "../assets/forca2.png";
+import forca3 from "../assets/forca3.png";
+import forca4 from "../assets/forca4.png";
+import forca5 from "../assets/forca5.png";
+import forca6 from "../assets/forca6.png";
 
-export default function Jogo({setHabilitarLetras, contarErros, palavras}){
+export default function Jogo({setHabilitarLetras, contarErros, palavras, palavraEscolhida, setPalavraEscolhida}){
+
+  const imagens = [forca0, forca1, forca2, forca3, forca4, forca5, forca6];
 
   function escolherPalavra(){
-    const palavra = (palavras[Math.floor(Math.random()*palavras.length)]).split('');
-    console.log(palavra);
-    return palavra;
+
+    const indiceAleatorio = Math.floor(Math.random()*palavras.length);
+    const palavra = palavras[indiceAleatorio].split('');
+    
+    setPalavraEscolhida([...palavra]);
   }
   function habilitarLetras(){
     setHabilitarLetras(false);
-    const palavraEscolhida = escolherPalavra();
+    escolherPalavra();
 
   }
     return (
+    <>
     <div className="jogo">
-      <img src={forca0} alt="imagem-forca"/>
+      <img src={imagens[contarErros]} alt="imagem-forca"/>
       <button onClick={habilitarLetras}>Escolher palavra</button>
     </div>
+     <div className="palavra">
+     {palavraEscolhida.map(letra => <div>_</div>)}
+    </div>
+    </>
     );
 }
